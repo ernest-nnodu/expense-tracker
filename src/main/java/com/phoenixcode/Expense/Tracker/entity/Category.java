08e9ef1e22dfb0b8cb260ac18c5d4641db672758
ernest nnodu <ernest.nnodu@outlook.com>
2025-06-26 15:45:16 +0100

    Create category entity

---
+++ src/main/java/com/phoenixcode/Expense/Tracker/entity/Category.java
@@ -0,0 +1,30 @@
+package com.phoenixcode.Expense.Tracker.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+
+}
