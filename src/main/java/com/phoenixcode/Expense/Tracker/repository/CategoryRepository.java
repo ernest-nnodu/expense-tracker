3757da09a381c8feec34655f3da8be801d05d7da
ernest nnodu <ernest.nnodu@outlook.com>
2025-06-26 16:02:05 +0100

    Create category repository

---
+++ src/main/java/com/phoenixcode/Expense/Tracker/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.phoenixcode.Expense.Tracker.repository;
+
+import com.phoenixcode.Expense.Tracker.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+}
