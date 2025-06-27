30e1d7a7a5bacd9d7e46a79aca610cb42e9c7cb3
ernest nnodu <ernest.nnodu@outlook.com>
2025-06-27 20:19:35 +0100

    Implement integration test to test get all categories endpoint call

--- src/test/java/com/phoenixcode/Expense/Tracker/controller/CategoryControllerIntegrationTest.java
+++ src/test/java/com/phoenixcode/Expense/Tracker/controller/CategoryControllerIntegrationTest.java
@@ -2,19 +2,17 @@ package com.phoenixcode.Expense.Tracker.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.phoenixcode.Expense.Tracker.dto.CreateCategoryRequestDto;
-import com.phoenixcode.Expense.Tracker.dto.CreateUserRequestDto;
 import com.phoenixcode.Expense.Tracker.repository.CategoryRepository;
-import com.phoenixcode.Expense.Tracker.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -54,20 +52,60 @@ public class CategoryControllerIntegrationTest {
     @Test
     @DisplayName("Create category endpoint call with existing category name unsuccessful")
     void createCategory_withExistingCategoryName_returns409StatusCode() throws Exception {
-        CreateCategoryRequestDto requestDto = createCategoryRequestDto();
-        mockMvc.perform(post("/api/categories")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(requestDto)));
 
-        CreateCategoryRequestDto existingUser = CreateCategoryRequestDto.builder()
+        saveCategory();
+        CreateCategoryRequestDto existingCategory = CreateCategoryRequestDto.builder()
                 .name("Shopping")
                 .description("description")
                 .build();
 
         mockMvc.perform(post("/api/categories")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(existingUser)))
+                        .content(objectMapper.writeValueAsString(existingCategory)))
                 .andExpect(status().isConflict());
+    }
+
+    @Test
+    @DisplayName("Create category endpoint call with blank category name unsuccessful")
+    void createCategory_withBlankCategoryName_returns400StatusCode() throws Exception {
+        CreateCategoryRequestDto requestDto = CreateCategoryRequestDto.builder()
+                .name("")
+                .description("description")
+                .build();
+
+        mockMvc.perform(post("/api/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestDto)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Get all categories returns 200 ok status")
+    void getAllCategories_returns200StatusCode() throws Exception {
+
+        mockMvc.perform(get("/api/categories")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+    
+    @Test
+    @DisplayName("Get all categories returns list of categories")
+    void getAllCategories_returnsListOfCategories() throws Exception {
+
+        saveCategory();
+
+        mockMvc.perform(get("/api/categories")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("[0].id").exists())
+                .andExpect(jsonPath("[0].name").value("Shopping"))
+                .andExpect(jsonPath("[0].description").value("Shopping description"));
+    }
+
+    private void saveCategory() throws Exception {
+        CreateCategoryRequestDto requestDto = createCategoryRequestDto();
+        mockMvc.perform(post("/api/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(requestDto)));
     }
 
     private CreateCategoryRequestDto createCategoryRequestDto() {
