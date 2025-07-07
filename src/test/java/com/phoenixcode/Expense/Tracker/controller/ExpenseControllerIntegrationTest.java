2231d4468b1dd2d0cf0feeb6e881a15eebd0c80b
ernest-nnodu <ernest.nnodu@outlook.com>
2025-07-07 10:20:44 +0100

    Rename expense integration test class name

--- src/test/java/com/phoenixcode/Expense/Tracker/controller/ExpenseControllerIntegrationTest.java
+++ src/test/java/com/phoenixcode/Expense/Tracker/controller/ExpenseControllerIntegrationTest.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class ExpenseCategoryIntegrationTest {
+public class ExpenseControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
