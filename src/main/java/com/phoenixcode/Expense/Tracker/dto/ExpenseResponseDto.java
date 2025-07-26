66763ec089643a75ba772839bb491864a016a912
ernest-nnodu <ernest.nnodu@outlook.com>
2025-07-26 23:00:02 +0100

    Configure documentation for expense response dto

--- src/main/java/com/phoenixcode/Expense/Tracker/dto/ExpenseResponseDto.java
+++ src/main/java/com/phoenixcode/Expense/Tracker/dto/ExpenseResponseDto.java
@@ -1,11 +1,13 @@
 package com.phoenixcode.Expense.Tracker.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Schema(description = "Expense response model")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -13,9 +15,18 @@ import java.util.UUID;
 @Builder
 public class ExpenseResponseDto {
 
+    @Schema(description = "Expense ID")
     private UUID id;
+
+    @Schema(description = "Expense amount")
     private BigDecimal amount;
+
+    @Schema(description = "Expense description")
     private String description;
+
+    @Schema(description = "Expense category")
     private String category;
+
+    @Schema(description = "Expense date")
     private LocalDate date;
 }
