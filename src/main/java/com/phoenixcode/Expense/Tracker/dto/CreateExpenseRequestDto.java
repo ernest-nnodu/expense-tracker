79a0efa6060fc3942dde14722bdc00fe88a808d6
ernest-nnodu <ernest.nnodu@outlook.com>
2025-07-26 23:14:43 +0100

    Configure documentation for create expense request dto

--- src/main/java/com/phoenixcode/Expense/Tracker/dto/CreateExpenseRequestDto.java
+++ src/main/java/com/phoenixcode/Expense/Tracker/dto/CreateExpenseRequestDto.java
@@ -1,6 +1,7 @@
 package com.phoenixcode.Expense.Tracker.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -9,6 +10,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Schema(description = "Create or update expense request model")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -16,19 +18,24 @@ import java.util.UUID;
 @Builder
 public class CreateExpenseRequestDto {
 
+    @Schema(description = "Expense amount")
     @NotNull(message = "Amount is required")
     private BigDecimal amount;
 
+    @Schema(description = "Expense description")
     @NotBlank(message = "Description must not be empty")
     private String description;
 
+    @Schema(description = "Expense date")
     @NotNull
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
 
+    @Schema(description = "Expense category ID")
     @NotNull(message = "Category is required")
     private UUID category;
 
+    @Schema(description = "Expense user ID")
     @NotNull(message = "User id is required")
     private UUID user;
 }
